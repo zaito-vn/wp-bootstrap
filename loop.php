b953a43013632c6d1b3095fadde9d5eed5cfaa29
Tran The Trong <trongtt@gmail.com>
2012-12-08 23:38:26 +0700

    Add the breadcrumb list

--- loop.php
+++ loop.php
@@ -8,6 +8,7 @@
 <?php endif; ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
+	<?php echo breadcrumb_lists(); ?>
 	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="page-header">
 			<h2 class="post-title">
