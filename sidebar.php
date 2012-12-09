ccb7f606843d23621a017c3cd65a7249c5a44c62
Tran The Trong <trongtt@gmail.com>
2012-12-10 00:13:31 +0700

    Stylize for widgets

--- sidebar.php
+++ sidebar.php
@@ -1,18 +1,17 @@
 <div id="primary" class="widget-area" role="complementary">
   <?php
 	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
-  <div class="well well-small">
+  <div class="widget well well-small">
     <h3 class="widget-title">Archives</h3>
     <ul>
       <?php wp_get_archives( 'type=monthly' ); ?>
     </ul>
   </div>
-  <div class="well well-small">
+  <div class="widget well well-small">
     <h3 class="widget-title">Meta</h3>
     <ul>
       <?php wp_register(); ?>
-      <li><?php wp_loginout(); ?>
-      </li>
+      <li><?php wp_loginout(); ?></li>
       <?php wp_meta(); ?>
     </ul>
   </div>
